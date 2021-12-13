720ce4e4cc6bc79edb712ef886284b6823529656
molaraiche <laraichemohamed@gmail.com>
2021-12-13 05:29:44 +0000

    firstpage

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
 "use strict";
 // Variables
-
+const burger = document.querySelector(".burger");
+const list = document.querySelector("ul");
 // Events
+
+burger.addEventListener("click", (e) => {
+  list.classList.toggle("active");
+  e.preventDefault();
+});
